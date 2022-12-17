67104cf0f6b603e871fc9e649e4469f665f572ea
hemilvp <113291204+hemilvp@users.noreply.github.com>
2022-12-17 21:48:22 +0530

    Add files via upload

---
+++ 4A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    if (a == 2 || a == 1 || a == 0)
+        cout << "NO" << endl;
+    else if (a % 2 == 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
