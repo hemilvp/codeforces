18d3a98be107b208733f68925fff21d96d5bc166
Patel Hemil Vipulkumar <113291204+hemilvp@users.noreply.github.com>
2022-12-27 09:07:52 +0530

    Add files via upload

---
+++ 1772A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int a,b;
+        char c;
+        cin >> a >> c >> b;
+        cout << a+b << endl;
+        
+    }
+    
+return 0;
+}
